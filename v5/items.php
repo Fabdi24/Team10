8ddd7cbc6b1568389fa686bd74c89cb64a323938
Hussain1244 <116089407+Hussain1244@users.noreply.github.com>
2022-11-30 17:24:29 +0000

    Admin able to view all products / orders

--- v5/items.php
+++ v5/items.php
@@ -38,203 +38,88 @@
             <i id="bar" class="fas fa-outdent"></i>
         </div>
     </section>
-    <div class="containerProduct">
-                <div class="card">
-                    <div class="card-header">
-                        <div class="itemHead">
-                        <h2>Products</h2>
-                    </div>
-                <class="card-body">
-                    <table class="table">
-                        <thead class="tHead">
-                            <tr>
-                                <th>Name</th>
-                                <th>Price</th>
-                                <th>Image</th>
-                                <th>Description</th>
-                                <th>Color</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody class="tBody">
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td><href></href></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td><href></href></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td><href></href></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td><href></href></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td><href></href></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td><href></href></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td><href></href></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td><href></href></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td><href></href></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td><href></href></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td><href></href></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td><href></href></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td><href></href></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td><href></href></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td><href></href></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td><href></href></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td><href></href></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td><href></href></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td><href></href></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td><href></href></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td><href></href></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td><href></href></td>
-                            </tr>
-                        </tbody>
+   
+    <?php
+
+    include "connect.php";
+    require "config.php";
+        
+        
+            $sql = ("SELECT id, name, description, colour, quantity, price FROM products");
+            $result = mysqli_query($conn, $sql);
+        echo"
+        <div style='margin-left:10%; margin-right:5%;  min-width:80vw; min-height: 20vw;'>
+        <div class ='scroll-div'>
+            <div class ='scroll-object'>
+                <table align ='center' border='10px' style='width:100%; min-width:800px; line-height:100px; background:grey'>
+                    <tr>
+                        <th colspan ='6'><h1> Products </h1></th>
+                        <th colspan ='6'>
+                        
+                        <form class='search', method='post' ,  autocomplete='off'>
+                            <input type='text' name='search' required/>
+                            <button type='searchSubmit'> Search </button>
+
+                        </form>
+                        
+                        
+                        </th>
+                    </tr>
+                    <t>
+                        <th> ID </th>
+                        <th> Name </th>
+                        <th> Description </th>
+                        <th> Colour </th>
+                        <th> Quantity </th>
+                        <th> Price(£) </th>
+                        
+                    </t>";
+                    
+                        if (isset($_POST['search'])){
+                            $name = $_POST['search'];
+                            $sql = ("SELECT id, name, description, colour, quantity, price FROM products where name = `$name`");
+                           
+                            $result = mysqli_query($conn, $sql);
+                            while($rows=mysqli_fetch_assoc($result)) {
+                                echo "
+                            <tr style='text-align: center;'>
+                            <td>{$rows['id']} </td>
+                            <td>{$rows['name']} </td>
+                            <td>{$rows['description']} </td>
+                            <td>{$rows['colour']} </td>
+                            <td>{$rows['quantity']} </td>
+                            <td>{$rows['price']} </td>";
+                            
+                        
+                            }
+                        }
+
+                    
+                        while($rows=mysqli_fetch_assoc($result)) 
+                        {
+                            echo "
+                        <tr style='text-align: center;'>
+                            
+                            <td>{$rows['id']} </td>
+                            <td>{$rows['name']} </td>
+                            <td>{$rows['description']} </td>
+                            <td>{$rows['colour']} </td>
+                            <td>{$rows['quantity']} </td>
+                            <td>{$rows['price']} </td>";
+                            
+                        }
+                            
+                    echo" </tr>
+                        
                     </table>
+                </div>
+            </div>
+        </div>";
+                    
+
+        
+
+        ?> 
                 <a href="addItems.php" class="btnproduct">Add Product</a>
         </class>
     </div>
--- v5/order.php
+++ v5/order.php
@@ -39,178 +39,82 @@
         </div>
     </section>
 
-    <div class="containerProduct">
-                <div class="card">
-                    <div class="card-header">
-                        <div class="orderHead">
-                        <h2>Orders</h2>
-                    </div>
-                <class="card-body">
-                    <table class="table">
-                        <thead class="tHead">
-                            <tr>
-                                <th>Customer ID</th>
-                                <th>First Name</th>
-                                <th>Last Name</th>
-                                <th>Products</th>
-                                <th>Delivery Status</th>
-                            </tr>
-                        </thead>
-                        <tbody class="tBody">
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </class>
-    </div>
-</div>
-</div>
+    <?php
 
+    include "connect.php";
+    require "config.php";
+        
+        
+            $sql = ("SELECT user_id, user_firstname, user_lastname, user_orders FROM user");
+            $result = mysqli_query($conn, $sql);
+        echo"
+        <div style='margin-left:10%; margin-right:5%;  min-width:80vw; min-height: 20vw;'>
+        <div class ='scroll-div'>
+            <div class ='scroll-object'>
+                <table align ='center' border='10px' style='width:100%; min-width:800px; line-height:100px; background:grey'>
+                    <tr>
+                        <th colspan ='3'><h1> Orders </h1></th>
+                        <th colspan ='3'>
+                        
+                        <form class='search', method='post' ,  autocomplete='off'>
+                            <input type='text' name='search' required/>
+                            <button type='searchSubmit'> Search </button>
+
+                        </form>
+                        
+                        
+                        </th>
+                    </tr>
+                    <t>
+                        <th> ID </th>
+                        <th> First Name </th>
+                        <th> Last Name </th>
+                        <th> Orders </th>
+                        
+                        
+                    </t>";
+                    
+                        if (isset($_POST['search'])){
+                            $name = $_POST['search'];
+                            $sql = ("SELECT user_id, user_firstname, user_lastname, user_orders FROM user where user_firstname = '$name' or user_lastname = '$name'");
+                            $result = mysqli_query($conn, $sql);
+                            while($rows=mysqli_fetch_assoc($result)) {
+                                echo "
+                            <tr style='text-align: center;'>
+                            <td>{$rows['user_id']} </td>
+                            <td>{$rows['user_firstname']} </td>
+                            <td>{$rows['user_lastname']} </td>
+                            <td>{$rows['user_orders']} </td>";
+                           
+                            
+                        
+                            }
+                        }
+
+                    
+                        while($rows=mysqli_fetch_assoc($result)) 
+                        {
+                            echo "
+                        <tr style='text-align: center;'>
+                            
+                            <td>{$rows['user_id']} </td>
+                            <td>{$rows['user_firstname']} </td>
+                            <td>{$rows['user_lastname']} </td>
+                            <td>{$rows['user_orders']} </td>";
+                            
+                        }
+                            
+                    echo" </tr>
+                        
+                    </table>
+                </div>
+            </div>
+        </div>";
+                    
+
+        
+
+        ?> 
 
     <section id="adminFooter" class="section-p1 section-m1">
         <div class="newstext">
