b19d4a56c370dd144b5848a231d9c1c807b3c67f
Fabdi24 <210083008@aston.ac.uk>
2022-12-01 15:43:27 +0000

    Update login.php

--- v5/login.php
+++ v5/login.php
@@ -1,31 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Account - Sign up or Login</title>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
+
+    <link rel="stylesheet" href="style.css">
+</head>
+      
+<body>
 <div id="id01" class="modal">
   
-  
-  
-      <div class="container" style="background-color:#f1f1f1">
-        <button type="button" onclick="document.getElementById('id02').style.display='none'" class="cancelbtn">Cancel</button>
-      </div>
-    </form>
-  </div>
-  <span></span>
-  <div id="id03" class="modal">
-  
-    <form class="modal-content animate" method="post" action ="auth.php">
-      <div class="imgcontainer">
-        <span onclick="document.getElementById('id03').style.display='none'" class="close" title="Close Modal">&times;</span>
-      </div>
-  
-      <div class="container">
-        <label for="uname"><b>Email</b></label>
-        <input type="text" placeholder="Email Address" name="email" required>
-  
-        <label for="psw"><b>Password</b></label>
-        <input type="password" placeholder="Password" name="password" required>
-          
-        <button type="submit">Login</button>
+  <form class="modal-content animate" action="/login.php" method="post">
+      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+    </div>
 
-      </div>
-</form>
+    <div class="container">
+      <label for="uname"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="uname" required>
+
+      <label for="psw"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" required>
+        
+      <button type="submit">Login</button>
+    </div>
+
+    <div class="container" style="background-color:#f1f1f1">
+      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+    </div>
+  </form>
+</div>
+<body>
