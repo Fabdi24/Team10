84a21c9aacfe2cbf895f627ac3110bc400463e3b
Fabdi24 <210083008@aston.ac.uk>
2022-12-01 17:04:59 +0000

    Update items.php

--- v6/items.php
+++ v6/items.php
@@ -20,7 +20,7 @@
         <div>
             <ul id="navbar">
                 <li><a href="index.php">Home</a></li>
-                <li><a href="admin.php">Dashboard</a></li>
+                <li><a href="dashboard.php">Dashboard</a></li>
                 <li><a href="customer.php">Customer</a></li>
                 <li><a href="order.php">Orders</a></li>
                 <li><a class="active" href="items.php">Products</a></li>
